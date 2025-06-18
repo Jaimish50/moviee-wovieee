4e8c62e6d35d6f7830a784ef6f7c398e20453be3
jaimish <jaimishdj151@gmail.com>
2025-06-18 18:37:42 +0530

    changes

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "📦 Installing dependencies from requirements.txt..."
-pip install -r requirements.txt
+# echo "📦 Installing dependencies from requirements.txt..."
+# pip install -r requirements.txt
 
-echo "🔁 Reinstalling eventlet (just to be safe)..."
-pip uninstall -y eventlet
-pip install --force-reinstall eventlet==0.33.3
+# echo "🔁 Reinstalling eventlet (just to be safe)..."
+# pip uninstall -y eventlet
+# pip install --force-reinstall eventlet==0.33.3
